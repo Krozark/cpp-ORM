8e24d48dfcb3abf73bde136a5d59d89abdab57b7
Krozark <maxime.barbier1991@gmail.com>
2013-06-07 10:59:41 +0200

    add FK

---
+++ ORM/FK.hpp
@@ -0,0 +1,34 @@
+#ifndef ORM_FK_HPP
+#define ORM_FK_HPP
+
+#include "VAttr.hpp"
+
+namespace orm
+{
+    class Query;
+
+    template<typename T>
+    class FK : public VAttr
+    {
+        FK(const int& id,const std::string& colum);
+        FK(const std::string& colum);
+
+        FK(const FK&) = delete;
+
+        virtual bool get(const Query& query);
+        typedef T type;
+
+        T& operator*(){return *value_ptr;};
+        T* operator->(){return value_ptr;};
+
+        protected:
+            int value;
+            T* value_prt;
+
+            virtual void print(std::ostream& output) const;
+            virtual bool set(Query& query,const unsigned int& colum) const;
+
+    };
+}
+
+#endif
