317dbcc257916f6cf1c0950772808043079575a2
Krozark <maxime.barbier1991@gmail.com>
2014-05-26 16:27:40 +0200

    change op namespace as class

--- include/ORM/backends/op.hpp
+++ include/ORM/backends/op.hpp
@@ -8,27 +8,28 @@ namespace orm
     /**
      * \brief Stor the operator values for filter and ordering
      */
-    namespace op
+    class op
     {
-        //filter
-        static const std::string exact      = "exact";
-        static const std::string iexact     = "iexact";
-        static const std::string contains   = "contains" ;
-        static const std::string icontains  = "icontains";
-        static const std::string regex      = "regex";
-        static const std::string iregex     = "iregex";
-        static const std::string gt         = "gt";
-        static const std::string gte        = "gte";
-        static const std::string lt         = "lt";
-        static const std::string lte        = "lte";
-        static const std::string startswith = "startswith";
-        static const std::string endswith   = "endswith";
-        static const std::string istartswith= "istartswith";
-        static const std::string iendswith  = "iendswith";
-        //ordering
-        static const char        random     = '?';
-        static const char        asc        = '+';
-        static const char        desc       = '-';
-    }
+        public:
+            //filter
+            static const std::string exact;
+            static const std::string iexact;
+            static const std::string contains;
+            static const std::string icontains;
+            static const std::string regex;
+            static const std::string iregex;
+            static const std::string gt;
+            static const std::string gte;
+            static const std::string lt;
+            static const std::string lte;
+            static const std::string startswith;
+            static const std::string endswith;
+            static const std::string istartswith;
+            static const std::string iendswith;
+            //ordering
+            static const char        random;
+            static const char        asc;
+            static const char        desc;
+    };
 }
 #endif
