17daf9efe03f305e681740f45fcacda06f058213
Krozark <maxime.barbier1991@gmail.com>
2014-07-24 11:28:10 +0200

    remove multi definition

--- src/ORM/backends/private/Filter.cpp
+++ src/ORM/backends/private/Filter.cpp
@@ -2,4 +2,16 @@
 
 namespace orm
 {
+    //special date type
+    template<>
+    struct tm __filter_value_helper<struct tm>(const struct tm& value)
+    {
+        tm tmp;
+        ::memcpy(&tmp,&value,sizeof(struct tm));
+        ::mktime(&tmp);
+        tmp.tm_year +=1900;
+        tmp.tm_mon +=1;
+        tmp.tm_isdst = -1;
+        return tmp;
+    }
 }
