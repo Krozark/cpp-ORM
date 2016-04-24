9d6aa4ec586b942d76535f8d7982faf0039ea1e1
Krozark <maxime.barbier1991@gmail.com>
2016-04-24 18:01:38 +0200

    set Sqlite as default for example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,9 @@
-#include <ORM/backends/MySql.hpp>
-orm::MySqlDB def("root","toor","test");
+//#include <ORM/backends/MySql.hpp>
+//orm::MySqlDB def("root","toor","test");
 //orm::MySqlDB def("root","root","test");
 
-/*#include <ORM/backends/Sqlite3.hpp>
-orm::Sqlite3DB def("./test.db");*/
+#include <ORM/backends/Sqlite3.hpp>
+orm::Sqlite3DB def("./test.db");
 
 orm::DB& orm::DB::Default = def;
 
